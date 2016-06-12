c4056606390f34bbf5058de2493ccfe98f19e521
Sam Vervaeck <sam@vervaeck.be>
2016-06-12 14:46:23 +0200

    Rename to ModuleInterpreter

---
+++ src/ModuleInterpreter.ts
@@ -0,0 +1,41 @@
+
+import * as fs from "fs-promise"
+import * as path from "path"
+import * as System from "systemjs"
+
+import { IModuleLoader } from "../interfaces/IModuleLoader"
+
+// TODO: currently using systemjs, might be worthwile not to depend on it
+class ModuleInterpreter implements IModuleLoader {
+
+  dir: string
+  system: System
+  extensions = ['.js', '.coffee', '.ts']
+
+  private async getFilePath(name: string): Promise<string> {
+    const files = []
+    const p = path.resolve(this.dir, name)
+    await Promise.all(this.extensions.map(async (ext) => {
+      const file = p+ext
+      if (await fs.access(file, fs.R_OK))
+        files.push(file)
+    }))
+    return files
+  }
+
+  constructor() {
+    this.system = new System.constructor()
+    const loader = this
+    this.system.locate = (load) => loader.getFilePath(load.name)
+  }
+
+  importModule(name: string): Promise<any> {
+    return this.system.import(name)
+  }
+
+  hasModule(name: string) { 
+    return this.getFilePath(name).then(path => !!path)
+  }
+
+}
+
