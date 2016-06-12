5915cd4035f75bb006180ec86ea0b0d1ddd0ef6f
Sam Vervaeck <sam@vervaeck.be>
2016-06-12 10:49:49 +0200

    Rename provides to provide to be consistent with inversify

--- annotations/provide.ts
+++ annotations/provide.ts
@@ -1,12 +1,23 @@
 
 /// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts" />
 
-export function provides(intf: string, name: string) {
+export function provide(intf: string) {
+  return (target) => {
+    if (Reflect.hasMetadata("interface", target))
+      throw new Error(`${target} already has an interface defined`)
+    Reflect.defineMetadata("provided", true, target)
+    Reflect.defineMetadata("interface", intf, target)
+    return target
+  }
+}
+
+export function provideNamed(intf: string, name: string) {
   if (!name)
     throw new Error(`no name given for interface instanciation`)
   return (target) => {
     if (Reflect.hasMetadata("interface", target))
       throw new Error(`${target} already has an interface defined`)
+    Reflect.defineMetadata("provided", true, target)
     Reflect.defineMetadata("interface", intf, target)
     Reflect.defineMetadata("name", name, target)
     return target
