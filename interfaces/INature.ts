4bfa161b6787d23a5af37a384051725ac0ed99d2
Sam Vervaeck <sam@vervaeck.be>
2016-06-19 22:07:27 +0200

    Move platform interfaces to comet-platform

--- interfaces/INature.ts
+++
@@ -1,9 +0,0 @@
-
-import { IRunnable } from "./IRunnable"
-import { INamedSet } from "./NamedSet"
-
-export interface INature {
-  getEntryPoints(): INamedSet<IRunnable>
-  getDefaultEntryPoint(): IRunnable
-}
-
