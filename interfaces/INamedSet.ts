734af2e3b9523c4338ff5ba10319b7a6471df133
Sam Vervaeck <sam@vervaeck.be>
2016-06-19 20:16:06 +0200

    Extend INamedSet with events

--- interfaces/INamedSet.ts
+++ interfaces/INamedSet.ts
@@ -1,9 +1,24 @@
 
 export interface INamedSet<T> {
+
   has(name: string)
+
   get(name: string)
+
   add(name: string, value: T)
+
   remove(name: string)
+
+  /**
+   * Emitted events:
+   *
+   * <ul>
+   *   <li>add</li>
+   *   <li>remove</li>
+   * </ul>
+   */
+  on(event: string, callback: Function)
+
   clear()
 }
 
