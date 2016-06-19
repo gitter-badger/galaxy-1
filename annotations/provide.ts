cec1b80ed9c6b3c46c242974855d80c646ad1de0
Sam Vervaeck <sam@vervaeck.be>
2016-06-19 21:59:23 +0200

    Depend on Runtime instead of IKernel

--- annotations/provide.ts
+++ annotations/provide.ts
@@ -2,13 +2,13 @@
 /// <reference path="../typings/index.d.ts" />
 /// <reference path="../node_modules/inversify-dts/inversify/inversify.d.ts" />
 
-import { IKernel } from "inversify"
+import { IRuntime } from "../interfaces/IRuntime"
 
-export function createProvideAnnotation(kernel: IKernel) {
+export function createProvideAnnotation(runtime: IRuntime) {
   // TODO: add generalized constraints
   return function provide(intf: Symbol, name?: string) {
     return (target) => {
-      const binding = kernel.bind(intf).to(target)
+      const binding = runtime.kernel.bind(intf).to(target)
       if (name)
         binding.whenTargetNamed(name)
       return target
