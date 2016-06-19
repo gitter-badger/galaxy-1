719944a5c7337131fe135831fdb08f5e74b09df4
Sam Vervaeck <sam@vervaeck.be>
2016-06-19 22:20:24 +0200

    Refactor names to be shorter

--- interfaces/IModuleLoader.ts
+++ interfaces/IModuleLoader.ts
@@ -2,7 +2,7 @@
 /// <reference path="../typings/index.d.ts" />
 
 export interface IModuleLoader {
-  importModule(name: string): Promise<any>
-  hasModule(name: string): Promise<boolean>
+  import(name: string): Promise<any>
+  has(name: string): Promise<boolean>
 }
 
