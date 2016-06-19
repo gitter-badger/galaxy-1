6a28c2f2ff61798fd3535095851e26db90237db6
Sam Vervaeck <sam@vervaeck.be>
2016-06-19 22:56:03 +0200

    Add directory app source for crun

--- bin/crun.ts
+++ bin/crun.ts
@@ -2,10 +2,24 @@
 
 /// <reference path="../typings/index.d.ts" />
 
+import * as path from "path"
+import { IApp } from "../interfaces/IApp"
+import { EventedNamedSet } from "../src/EventedNamedSet"
+import { AppDirWatcher } from "../src/AppDirSource"
 import { Runtime } from "../src/Runtime"
+import { AppDirSet } from "../src/AppDirSet"
 import * as minimist from "minimist"
 
 const argv = minimist(process.argv.slice(2))
 
+if (!argv._[0])
+  throw new Error(`must specify a directory`)
+
+const apps = new EventedNamedSet<IApp>()
+const watcher = AppDirWatcher(apps, path.resolve(argv._[0]))
+
+const runtime = new Runtime({
+  apps: apps 
+})
 
 
