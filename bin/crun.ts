23ab20ecd789851bbe931bde3b1c77576c6bba73
Sam Vervaeck <sam@vervaeck.be>
2016-06-19 22:26:17 +0200

    Add crun executable

---
+++ bin/crun.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env NODE
+
+/// <reference path="../typings/index.d.ts" />
+
+import { Runtime } from "../src/Runtime"
+import * as minimist from "minimist"
+
+const argv = minimist(process.argv.slice(2))
+
+
+
