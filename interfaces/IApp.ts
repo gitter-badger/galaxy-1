3a5c84ce673dc12f56c327a055d260c2151fa542
Sam Vervaeck <sam@vervaeck.be>
2016-06-22 21:14:26 +0200

    Make components partof app

--- interfaces/IApp.ts
+++ interfaces/IApp.ts
@@ -3,7 +3,8 @@ import { INamedSet } from "./INamedSet"
 import { IComponent } from "./IComponent"
 import { IModuleLoader } from "./IModuleLoader"
 
-export interface IApp extends INamedSet<IComponent> {
+export interface IApp {
   loader: IModuleLoader
+  components: INamedSet<IComponent>
 }
 
